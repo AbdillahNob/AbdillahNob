d7645baae3b46aa7e32ff9c1584261950f0755ad
Abdillah P Al-Iman <93828455+AbdillahNob@users.noreply.github.com>
2025-01-17 00:34:21 +0800

    Add files via upload

---
+++ bio.js
@@ -0,0 +1,28 @@
+const bio = [
+  {
+    nama: "Abdillah",
+    profesi: "undifined",
+    status: "Pemuda Intlektual",
+    asal: "Kab.Luwu Utara",
+    religion: "Muslim",
+    hobi: [
+      {
+        indoor: "Watching anime and Movie",
+        outdoor: "Playing Basketball",
+      },
+      {
+        indoor: "Main Game",
+        outdoor: "Gardening",
+      },
+      {
+        indoor: "Cooking",
+        outdoor: "Hike and Explore Nature",
+      },
+    ],
+    Motivation: () => {
+      today < yesterday ? self.improve() : keepGoing();
+    },
+    motto:
+      "Jangan buang waktu masa muda mu dengan sesuatu yang tidak bermanfaat !, gunakanlah utk menuntut ilmu dan bekerja keras.",
+  },
+];
